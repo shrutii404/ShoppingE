1fe0782200a0879ebe129ad6cccd7c29b3d5c4aa
Shruti <shrutisharma1575@gmail.com>
2025-03-18 20:43:36 +0530

    platform

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,9 +7,9 @@ const Footer = () => {
         <div className="d-flex align-items-center justify-content-center pb-5">
           <div className="col-md-6">
             <p className="mb-3 mb-md-0">Made with ❤️ by {" "}
-              <a  href="https://sahibsingh.dev" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">Sahib Singh</a>
+              <a  href=" " className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">Shruti Sharma</a>
             </p>
-            <a className="text-dark fs-4" href="https://github.com/ssahibsingh" target="_blank" rel="noreferrer">
+            <a className="text-dark fs-4" href="https://github.com/shrutii404/shrutii404" target="_blank" rel="noreferrer">
               <i className="fa fa-github"></i>
             </a>
           </div>
